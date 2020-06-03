649b29d0ea1069c7a3009a29d1d37c7435963496
Chris Sanchez <chrisvsanchez@gmail.com>
2020-06-03 19:26:05 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,8 +2,8 @@ def using_concat(array1, array2)
   array1.concat(array2)
 end
 
-def using_insert(array)
-  array.insert
+def using_insert(array, ele)
+  array.insert(4,ele)
 end
 
 def using_uniq(array)
